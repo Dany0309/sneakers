23b8d4c9b111c680eada14edad858944e08d859c
Dany0309 <142040968+Dany0309@users.noreply.github.com>
2023-12-17 16:45:54 +0500

    Урок 2 сделали компоненты

--- src/App.js
+++ src/App.js
@@ -1,95 +1,33 @@
+import Card from "./components/Card";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="wrapper clear">
+      {/* Корзина */}
+      <Drawer/>
       {/* Шапка сайта */}
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-          <img
-            width={40}
-            height={40}
-            className="mr-15"
-            src="./img/logo.png"
-            alt=""
-          />
-          <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
-          </div>
-        </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img
-              width={18}
-              height={18}
-              className="mr-10"
-              src="./img/box.svg"
-              alt=""
-            />
-            <span>1205 руб.</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="./img/user.svg" alt="" />
-          </li>
-        </ul>
-      </header>
+      <Header/>
       {/* Основная часть */}
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
-
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          {/* Поиск */}
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
         {/* Карточки */}
         <div className="d-flex">
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          
+
+          
         </div>
         {/* Конец карточек */}
       </div>
